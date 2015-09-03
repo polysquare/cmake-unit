8e18f46fa955ae29293469569bfd76564ac677bb
Sam Spilsbury <smspillaz@gmail.com>
2015-09-03 09:38:56 +0800

    Use forwarded arguments in _cmake_unit_configure_test_internal

--- CMakeUnitRunner.cmake
+++ CMakeUnitRunner.cmake
@@ -1228,10 +1228,9 @@ function (_cmake_unit_configure_test_internal)
                                          ${ARGN})
 
     set (PHASE ${_CMAKE_UNIT_PHASE})
-    set (TEST_SOURCE_DIR "${CMAKE_UNIT_CONFIGURE_TEST_SOURCE_DIR}")
-    set (TEST_BINARY_DIR "${CMAKE_UNIT_CONFIGURE_TEST_BINARY_DIR}")
 
-    cmake_forward_arguments (TEST PHASE_FUNCTION_STANDARD_ARGS
+    cmake_forward_arguments (CMAKE_UNIT_CONFIGURE_TEST
+                             PHASE_FUNCTION_STANDARD_ARGS
                              SINGLEVAR_ARGS SOURCE_DIR
                                             BINARY_DIR
                                             OUTPUT_FILE
@@ -1239,10 +1238,9 @@ function (_cmake_unit_configure_test_internal)
 
     cmake_call_function (${PHASE_FUNCTION} ${PHASE_ARGUMENTS}
                          TEST_NAME ${TEST_NAME}
-                         SOURCE_DIR "${TEST_SOURCE_DIR}"
-                         BINARY_DIR "${TEST_BINARY_DIR}"
                          OUTPUT_FILE "${TEST_BINARY_DIR}/${PHASE}.output"
-                         ERROR_FILE "${TEST_BINARY_DIR}/${PHASE}.error")
+                         ERROR_FILE "${TEST_BINARY_DIR}/${PHASE}.error"
+                         ${PHASE_FUNCTION_STANDARD_ARGS})
 
     # Implicitly dereference _CMAKE_UNIT_PHASE_AFTER_${PHASE} and if there's
     # a phase to go to, recursively call this function and enter the next phase.
